e00e31b6e51683db67e4dcc9d0dd09508c33e0e9
wychoi <soc09072@gmail.com>
2024-11-13 10:27:08 +0900

    일단 파일만 만들어 두기

--- src/api/chat/index.js
+++ src/api/chat/index.js
@@ -1,8 +1,11 @@
 import Router from 'koa-router';
-import { justrainit } from './justrainit.ctrl.js';
+import { justrainit } from './quizrainit.ctrl.js';
 
 const chat = new Router();
 
 chat.post('/just', justrainit);
+chat.post('/see', seerainit);
+chat.post('/magic', magicrainit);
+chat.post('/quiz', quizrainit);
 
 export default chat;
