e914701f3c378d012d9fa7ea15bd30da36859062
wychoi <soc09072@gmail.com>
2024-12-06 17:30:04 +0900

    Enhance chat response format and logging for improved JSON parsing

--- src/api/chat/seerainit.ctrl.js
+++ src/api/chat/seerainit.ctrl.js
@@ -5,7 +5,7 @@ dotenv.config();
 
 const index = async (req, res) => {
     try{
-        const { userMessage } = req.body;
+        const { userMessage, userMessageLog } = req.body;
         // openai api로 부터 매치메이킹 결과 받아오기
         const response = await axios.post(
             'https://api.openai.com/v1/chat/completions',
@@ -21,6 +21,11 @@ const index = async (req, res) => {
                     사용자와 재미있게 영어 대화를 해줘. \
                     창의력 있고 즐거운 대화를 해줘. 영어가 익숙치 않은 대한민국 학생을 위해쉬운 대화를 해줘.\
                     한번에 너무 길게 말하지말고 한줄씩 대답해줘. \
+                    답변은 {"rainit": "사과가 좋아요. 당근보다요."} 이런식으로 답변해줘. \
+                    채팅앱에서 json으로 파싱해야하거든.\
+                    메세지 로그를 줄껀데 {[{"sender": "user","text": "사용자의 말", "timestamp": "2024-12-06T12:00:03Z"}, {"sender": "rainit", "text": "rainit의 말", "timestamp": "2024-12-06T12:01:03Z"}, ...]} 이런식으로 주어져\
+                    저걸 파싱해서 이해하고 대답하면 돼.\
+                    timestamp가 적용되지 않으면 "timestemp":null로 지정할껀데 어차피 너 대화로그 이해하는데 타임스템프 필요없잖아. 무시하면돼.\
                     '
                 },
                 {
