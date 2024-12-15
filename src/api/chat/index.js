e76805b286613f4366a7ff8552f63cf4b25f1f82
wychoi <soc09072@gmail.com>
2024-12-15 22:39:00 +0900

    Refactor chat routes and implement new sea initialization logic

--- src/api/chat/index.js
+++ src/api/chat/index.js
@@ -1,13 +1,13 @@
 import express from 'express';
 import quizrainit from './quizrainit.ctrl.js';
 import justrainit from './justrainit.ctrl.js';
-import seerainit from './seerainit.ctrl.js';
+import searainit from './seerainit.ctrl.js';
 import magicrainit from './magicrainit.ctrl.js';
 
 const chat = express.Router();
 
 chat.post('/just', justrainit);
-chat.post('/see', seerainit);
+chat.post('/sea', searainit);
 chat.post('/magic', magicrainit);
 chat.post('/quiz', quizrainit);
 
